f08b0e70a905f4430ed47fd4a646accedf55f4e3
Ivan Morozov <frostow12h@yandex.ru>
2018-05-14 02:33:06 +0300

    Some new tests. Added history target but not checked.

--- src/l4/DieselTruck.java
+++ src/l4/DieselTruck.java
@@ -2,21 +2,17 @@ package l4;
 
 import java.util.Comparator;
 
-class DieselTruck extends Car implements Comparator<DieselTruck> {
+class DieselTruck extends Car {
 
-    DieselTruck(Shorty own, Position position, double volumeOfFuel) {
+    public DieselTruck(Shorty own, Position position, double volumeOfFuel) {
         super(own, position, volumeOfFuel);
     }
 
-    String putFruit(Fruit fr) {
+    public String putFruit(Fruit fr) {
         return fr.lang() + " теперь в кузове!";
     }
 
     public String toString() {
         return "Грузовая машина " + super.getHolder();
-    }
-
-    public int compare(DieselTruck d1, DieselTruck d2) {
-        return d1.getNumber().compareTo(d2.getNumber());
     }
 }
