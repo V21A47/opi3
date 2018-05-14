2388883099849f5e9f0730cf3990c4248e083d1a
Ivan Morozov <frostow12h@yandex.ru>
2018-05-14 15:09:15 +0300

    some tests were repaired

--- src/Test/HumanTest.java
+++ src/Test/HumanTest.java
@@ -4,50 +4,61 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class HumanTest{
+    private Human a = null;
+
+    @Before
+    public void setHuman(){
+        a = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
+    }
 
     @Test
-    public void moveToPosition(){
-        Human a = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
-        Position newPos1 = new Position(100, 100, 100);
-        Position newPos2 = new Position(0, 0, 0);
-
+    public void moveToDifferentPositions(){
         a.moveTo(new Position(100, 100, 100));
-
-        assertEquals(newPos1, a.getPosition());
+        assertEquals("Can't move to 100,100,100", new Position(100, 100, 100), a.getPosition());
 
         a.moveTo(new Position(0, 0, 0));
+        assertEquals("Can't move to 0,0,0", new Position(0, 0, 0), a.getPosition());
 
-        assertEquals(newPos2, a.getPosition());
+        a.moveTo(new Position(-100, -100, -100));
+        assertEquals("Can't move to -100,-100,-100", new Position(-100, -100, -100), a.getPosition());
+
+        a.moveTo(new Position(1, 2, 3));
+        assertEquals("Can't move to 1,2,3", new Position(1, 2, 3), a.getPosition());
+
+        a.moveTo(new Position(25, -12, 1000));
+        assertEquals("Can't move to 25,-12,1000", new Position(25, -12, 1000), a.getPosition());
+
+        a.moveTo(new Position(-1, -234, -10000));
+        assertEquals("Can't move to -1,-234,-10000", new Position(-1, -234, -10000), a.getPosition());
     }
 
     @Test
-    public void setPosition(){
-        Human a = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
+    public void setPositionWorks(){
         Position newPos = new Position(250, 100, 0);
-
         a.setPosition(new Position(250, 100, 0));
 
-        assertEquals(newPos, a.getPosition());
+        assertEquals("Position wasn't set!", newPos, a.getPosition());
     }
 
     @Test
-    public void setState(){
-        Human a = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
-
+    public void setStateWorks(){
         a.setState(HumanState.NOFEARLESS);
+        assertEquals("State wasn't set to NOFEARLESS", HumanState.NOFEARLESS, a.getState());
 
-        assertEquals(HumanState.NOFEARLESS, a.getState());
+        a.setState(HumanState.FEAR);
+        assertEquals("State wasn't set to FEAR", HumanState.FEAR, a.getState());
     }
 
     @Test
-    public void equals(){
-        Human a = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
-        Human b = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
+    public void equalsWorks(){
+        Human h1 = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
+        Human h2 = new Human("Abvgdezh", HumanState.NORMAL, new Position(0, 0, 0));
 
-        assertTrue(a.equals(a));
-        assertTrue(a.equals(b));
-        assertTrue(b.equals(a));
+        assertTrue("h1 is not equals to h1", h1.equals(h1));
+        assertTrue("h1 is not equals to h2", h1.equals(h2));
+        assertTrue("h2 is not equals to h1", h2.equals(h1));
     }
 }
--- src/Test/PlaceTest.java
+++ src/Test/PlaceTest.java
@@ -4,18 +4,29 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class PlaceTest{
+    private Place pl1 = null;
+    private Place pl2 = null;
+
+    @Before
+    public void initPlaces(){
+        pl1 = new Place("place!");
+        pl2 = new Place("place!");
+    }
+
 
     @Test
-    public void equals(){
-        Place pl1 = new Place("place!");
-        Place pl2 = new Place("place!");
+    public void equalsJustByConstructors(){
+        assertTrue("Equals to itself doesn't work!", pl1.equals(pl1));
+        assertTrue("pl1 is not equals to pl2!", pl1.equals(pl2));
+        assertTrue("pl2 is not equals to pl1!", pl2.equals(pl1));
+    }
 
-        assertTrue(pl1.equals(pl1));
-        assertTrue(pl1.equals(pl2));
-        assertTrue(pl2.equals(pl1));
 
+    @Test
+    public void equalsUsingElements(){
         try {
             pl1.addPremise(Terrain.HOUSE, 1, new Position(20, 0, 0));
             pl2.addPremise(Terrain.HOUSE, 1, new Position(20, 0, 0));
@@ -23,20 +34,21 @@ public class PlaceTest{
             System.out.println("Everything is bad");
         }
 
-        assertTrue(pl1.equals(pl1));
-        assertTrue(pl1.equals(pl2));
-        assertTrue(pl2.equals(pl1));
+        assertTrue("Equals to itself doesn't work!", pl1.equals(pl1));
+        assertTrue("pl1 is not equals to pl2!", pl1.equals(pl2));
+        assertTrue("pl2 is not equals to pl1!", pl2.equals(pl1));
     }
 
+
     @Test
-    public void addPremise(){
+    public void addPremiseAddedObjectCanBeAdded(){
         Place pl = new Place("place!");
 
         try{
             pl.addPremise(Terrain.HOUSE, 1, new Position(20, 0, 0));
         } catch (NoFreeSpaceExeption e) {}
 
-        assertEquals(Terrain.HOUSE, pl.getObject(1));
+        assertEquals("Added object is not in terrain!", Terrain.HOUSE, pl.getObject(1));
     }
 
     @Test(expected = NoFreeSpaceExeption.class)
